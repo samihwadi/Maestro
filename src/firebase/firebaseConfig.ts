7ca2ce94f8d9c9005d50f202e2308767112df98f
Samih Wadi <samihamaar@gmail.com>
2024-10-07 18:00:04 -0400

    Removed analytics

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -14,6 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); 
 export const storage = getStorage(app);
 
